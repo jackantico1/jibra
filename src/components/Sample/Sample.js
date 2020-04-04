b2cdb611a3a622812f0d3e2f9165567060bfca91
Jack Antcio <anticoj@bc.edu>
2020-04-04 11:43:17 -0400

    Started Cajun Connection

--- src/components/Sample/Sample.js
+++ src/components/Sample/Sample.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Sample.css';
+import CajunConnectionScreen from '../../assets/cajunConnectionScreen1.png';
 
 class Sample extends Component {
 
@@ -7,6 +8,18 @@ class Sample extends Component {
     return (
       <div className="sample">
         <h1 className="title">{this.props.title}</h1>
+        <div className="containerOne">
+          <div className="image">
+            <img  src={CajunConnectionScreen} alt="Cajun Connection Screenshot"/>
+          </div>
+          <div>
+            <p className="text">
+              Cajun Connection is a social media app made for young proffesionals to give ananayous feedback on each other's resumes,
+              portfolio's, and proffesional work. It allows users to create profiles and form connections with other students/young 
+              proffesionals interested in growing their network.
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
